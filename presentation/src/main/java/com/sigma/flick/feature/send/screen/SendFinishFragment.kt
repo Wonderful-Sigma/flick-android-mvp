86fce734cdb04b88642ac76b8b20563bd251e381
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-11-24 19:55:48 +0900

    :bug: :: fix alarm logic

--- presentation/src/main/java/com/sigma/flick/feature/send/screen/SendFinishFragment.kt
+++ presentation/src/main/java/com/sigma/flick/feature/send/screen/SendFinishFragment.kt
@@ -1,23 +1,25 @@
 package com.sigma.flick.feature.send.screen
 
 import android.content.Context
+import android.util.Log
 import android.widget.Toast
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.sigma.data.network.dto.account.MessageBodyRequest
+import com.sigma.data.network.dto.account.RemitRequest
 import com.sigma.flick.R
 import com.sigma.flick.base.BaseFragment
 import com.sigma.flick.databinding.FragmentSendFinishBinding
 import com.sigma.flick.feature.send.viewmodel.SendViewModel
 import com.sigma.flick.feature.user.viewmodel.UserViewModel
+import com.sigma.flick.main.toDecimalFormat
 import com.sigma.flick.utils.fadeIn
 import com.sigma.flick.utils.setDeleteBottomNav
 import com.sigma.flick.utils.slideUpAndFadeIn
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
 
 @AndroidEntryPoint
 class SendFinishFragment :
@@ -29,39 +31,49 @@ class SendFinishFragment :
     private lateinit var context: Context
 
     override fun start() {
+        Log.d("SendFinishFragment", "start")
         setDeleteBottomNav(activity)
         context = requireContext()
 
         val accountName = viewModel.depositAccountName.value
         val sendCoin = viewModel.sendCoin.value.toString()
 
+        val remittanceAccount = userViewModel.myInfo.value!!.account[0].id
+        val depositAccount = viewModel.depositAccountId.value!!
+        val sendMoney = viewModel.sendCoin.value!!.toLong()
+
         val tvFinishTitle = binding.tvFinishTitle
         tvFinishTitle.text = "${accountName}님에게\n${sendCoin}코인을 보낼게요"
         tvFinishTitle.slideUpAndFadeIn(context)
 
-        runBlocking {
-            lifecycleScope.launch {
-                delay(1000)
-            }
+        lifecycleScope.launch {
+            delay(1000)
+        }
 
-            lifecycleScope.launch {
-                viewModel.sendState.collect {
-                    if (it.isSuccess) {
-                        val accountId = viewModel.depositAccountId.value
-                        viewModel.postAlarm(
-                            accountId.toString(),
-                            MessageBodyRequest(
-                                "코인을 받았습니다",
-                                userViewModel.myInfo.value!!.name + "에게 ${sendCoin}코인을 받았습니다"
-                            )
+
+        lifecycleScope.launch{
+            viewModel.remit(RemitRequest(remittanceAccount, sendMoney, depositAccount)).join()
+            userViewModel.getUserInfo()
+        }
+
+        lifecycleScope.launch {
+            viewModel.sendState.collect {
+                Log.d("송금", "송금")
+                if (it.isSuccess) {
+                    val accountId = viewModel.depositAccountId.value
+                    viewModel.postAlarm(
+                        accountId.toString(),
+                        MessageBodyRequest(
+                            "코인을 받았습니다",
+                            userViewModel.myInfo.value!!.name + "에게 ${sendCoin.toDecimalFormat()}코인을 받았습니다"
                         )
-                        tvFinishTitle.text = "${accountName}님에게\n${sendCoin}코인을 보냈어요"
-                    }
-                    if (it.error.isNotEmpty()) {
-                        binding.animationCompletion.setAnimation(R.raw.animation_warning)
-                        tvFinishTitle.text = "${accountName}님에게\n${sendCoin}코인 보내기를\n실패했어요"
-                        Toast.makeText(context, it.error, Toast.LENGTH_SHORT).show()
-                    }
+                    )
+                    tvFinishTitle.text = "${accountName}님에게\n${sendCoin.toDecimalFormat()}코인을 보냈어요"
+                }
+                if (it.error.isNotEmpty()) {
+                    binding.animationCompletion.setAnimation(R.raw.animation_warning)
+                    tvFinishTitle.text = "${accountName}님에게\n${sendCoin.toDecimalFormat()}코인 보내기를\n실패했어요"
+                    Toast.makeText(context, it.error, Toast.LENGTH_SHORT).show()
                 }
             }
         }
--- presentation/src/main/java/com/sigma/flick/feature/send/screen/SendLoadFragment.kt
+++ presentation/src/main/java/com/sigma/flick/feature/send/screen/SendLoadFragment.kt
@@ -1,23 +1,20 @@
 package com.sigma.flick.feature.send.screen
 
 import android.os.Handler
+import android.util.Log
 import androidx.fragment.app.activityViewModels
-import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
-import com.sigma.data.network.dto.account.RemitRequest
 import com.sigma.flick.R
 import com.sigma.flick.base.BaseFragment
 import com.sigma.flick.databinding.FragmentSendLoadBinding
 import com.sigma.flick.feature.send.viewmodel.SendViewModel
-import com.sigma.flick.feature.user.viewmodel.UserViewModel
+import com.sigma.flick.main.toDecimalFormat
 import com.sigma.flick.utils.setDeleteBottomNav
-import kotlinx.coroutines.launch
 
 class SendLoadFragment :
     BaseFragment<FragmentSendLoadBinding, SendViewModel>(R.layout.fragment_send_load) {
 
     override val viewModel: SendViewModel by activityViewModels()
-    private val userViewModel: UserViewModel by activityViewModels()
 
     override fun start() {
         setDeleteBottomNav(activity)
@@ -25,20 +22,11 @@ class SendLoadFragment :
         val accountName = viewModel.depositAccountName.value
         val sendCoin = viewModel.sendCoin.value.toString()
 
-        val remittanceAccount = userViewModel.myInfo.value!!.account[0].id
-        val depositAccount = viewModel.depositAccountId.value!!
-        val sendMoney = viewModel.sendCoin.value!!.toLong()
 
-        lifecycleScope.launch{
-            viewModel.remit(RemitRequest(remittanceAccount, sendMoney, depositAccount)).join()
-            userViewModel.getUserInfo()
-        }
-
-
-
-        binding.tvLoadTitle.text = "${accountName}님에게\n${sendCoin}코인을 보낼게요"
+        binding.tvLoadTitle.text = "${accountName}님에게\n${sendCoin.toDecimalFormat()}코인을 보낼게요"
 
         Handler().postDelayed({
+            Log.d("다음","다음")
             val action = SendLoadFragmentDirections.toSendFinishFragment()
             findNavController().navigate(action)
         }, 1000L)
