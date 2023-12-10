b0f2ae356f5406d9d201d784b2d361256ae0a3f4
stev3j <mygajeong@gmail.com>
2023-12-10 22:02:09 +0900

    UPDATE : version update

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.wonderfulsigma.flick"
         minSdk = 26
         targetSdk = 34
-        versionCode = 8
-        versionName = "0.1.5"
+        versionCode = 9
+        versionName = "0.2.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
